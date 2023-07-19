574482b1dcb7c7d29dcdaf8144bd6196a90d7fa7
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-19 03:39:50 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/service-principal-delegated-permission-grant/index.ts
+++ src/service-principal-delegated-permission-grant/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/azuread/2.40.0/docs/resources/service_principal_delegated_permission_grant
 // generated from terraform resource schema
 
