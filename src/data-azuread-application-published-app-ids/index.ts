4c90f4caccf28673e7ecd3d6673c99fce3018ad7
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-10-04 14:06:28 +0000

    chore(deps): Updated  by cdktf/cdktf-repository-manager@95054db

--- src/data-azuread-application-published-app-ids/index.ts
+++ src/data-azuread-application-published-app-ids/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azuread/2.43.0/docs/data-sources/application_published_app_ids
 // generated from terraform resource schema
 
