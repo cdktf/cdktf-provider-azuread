5897a777d7466308b9dc5c662b3ee27882e6cc0d
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-18 12:26:56 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/administrative-unit-role-member/index.ts
+++ src/administrative-unit-role-member/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/azuread/2.47.0/docs/resources/administrative_unit_role_member
 // generated from terraform resource schema
 
