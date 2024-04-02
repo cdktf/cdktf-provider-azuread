fe8a99e5db54bcfdfe5e72f843769ebfb8fdd3f2
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-04-02 13:15:31 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@c6606f6

--- src/application-federated-identity-credential/index.ts
+++ src/application-federated-identity-credential/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azuread/2.47.0/docs/resources/application_federated_identity_credential
 // generated from terraform resource schema
 
