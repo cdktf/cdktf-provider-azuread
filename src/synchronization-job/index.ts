93a764353b875c7bb5aa3c9b331de912f18fbb87
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-05-27 09:19:47 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/synchronization-job/index.ts
+++ src/synchronization-job/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/azuread/2.50.0/docs/resources/synchronization_job
 // generated from terraform resource schema
 
