45e6d073c48f81bd207a2a2a0746500e7702b886
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-05-27 08:21:45 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@2c5c1f5

--- src/application-known-clients/index.ts
+++ src/application-known-clients/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azuread/2.50.0/docs/resources/application_known_clients
 // generated from terraform resource schema
 
