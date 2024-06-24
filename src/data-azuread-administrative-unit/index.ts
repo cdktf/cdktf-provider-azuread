7f1f012d609958b5f3864ac53bcbd428e6782231
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-24 12:09:48 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@6160473

--- src/data-azuread-administrative-unit/index.ts
+++ src/data-azuread-administrative-unit/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azuread/2.52.0/docs/data-sources/administrative_unit
 // generated from terraform resource schema
 
