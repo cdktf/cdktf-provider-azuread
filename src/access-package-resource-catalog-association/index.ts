828259adb38b77c441fbd06d5d501f28f4abc12a
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-22 21:47:30 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/access-package-resource-catalog-association/index.ts
+++ src/access-package-resource-catalog-association/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/azuread/2.53.1/docs/resources/access_package_resource_catalog_association
 // generated from terraform resource schema
 
