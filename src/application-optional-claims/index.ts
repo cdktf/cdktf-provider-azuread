e051e1e8827491f64c58869e7dd346080a2bab0d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-03 08:50:38 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@6f08e61

--- src/application-optional-claims/index.ts
+++ src/application-optional-claims/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azuread/2.53.1/docs/resources/application_optional_claims
 // generated from terraform resource schema
 
