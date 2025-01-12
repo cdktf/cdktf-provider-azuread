5b1457f1536e47afaf46e0439a1fd587e13ace98
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-12 05:46:57 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/application-known-clients/index.ts
+++ src/application-known-clients/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/azuread/3.0.2/docs/resources/application_known_clients
 // generated from terraform resource schema
 
