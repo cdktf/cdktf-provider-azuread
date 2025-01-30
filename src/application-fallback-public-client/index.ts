f1a8ca5207df2b25b17979980d1bd6c0ba62558f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-30 10:16:01 +0000

    chore(deps): update  jsii & typescript version to

--- src/application-fallback-public-client/index.ts
+++ src/application-fallback-public-client/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azuread/3.1.0/docs/resources/application_fallback_public_client
 // generated from terraform resource schema
 
