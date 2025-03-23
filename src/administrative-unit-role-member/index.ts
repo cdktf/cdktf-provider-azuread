d472d2ace7d2f290203135f2c973ca677ad2f200
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-23 05:43:28 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4c0813b

--- src/administrative-unit-role-member/index.ts
+++ src/administrative-unit-role-member/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azuread/3.2.0/docs/resources/administrative_unit_role_member
 // generated from terraform resource schema
 
--- src/authentication-strength-policy/index.ts
+++ src/authentication-strength-policy/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/azuread/3.2.0/docs/resources/authentication_strength_policy
 // generated from terraform resource schema
 
